d54dfa68460f2c17c704eaedaa6f7e44d20ea169
patrickfv <patrick.fv@outlook.com>
2021-01-05 02:15:58 -0300

    implement TextButton component

---
+++ app/components/TextButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+export default function TextButton({ title='TextButton', style, action }) {
+    
+    return (
+        <TouchableOpacity style={styles.container} onPress={action}>
+            <Text {...{ style }}>{ title }</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
