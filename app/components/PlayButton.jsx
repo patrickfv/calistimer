cfbd116c3658feea367e3a5ff194b209b7556054
patrickfv <patrick.fv@outlook.com>
2021-01-30 23:22:01 -0300

    create PlayButton

---
+++ app/components/PlayButton.jsx
@@ -0,0 +1,62 @@
+import React, { useState, } from 'react'
+import { View, StyleSheet, TouchableOpacity, } from 'react-native'
+import Svg, { Path, G, Circle, } from 'react-native-svg'
+import { useSharedValue, useAnimatedStyle, } from 'react-native-reanimated'
+import Animated from 'react-native-reanimated'
+
+function Icon() {
+    const offset = useSharedValue(0)
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ rotateY: `${offset.value}deg` }]
+        }
+    })
+
+    const onTouchStart = () => {
+
+    }
+
+    return (
+        <Animated.View style={animatedStyle} {...{ onTouchStart }} >
+            <Svg width="148" height="148" viewBox="0 0 148 148" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <Circle cx="74" cy="74" r="70.5" fill="white" stroke="black" strokeWidth="7"/>
+                <Path d="M116.594 68.1605C121.286 70.5547 121.331 77.2429 116.671 79.6999L57.1053 111.111C52.7936 113.385 47.6063 110.279 47.5735 105.405L47.1565 43.3868C47.1238 38.5125 52.2689 35.3378 56.6107 37.5533L116.594 68.1605Z" fill="white" stroke="black" strokeWidth="7"/>
+                <Path d="M116.671 79.6999L57.1053 111.111C52.7936 113.385 47.6063 110.279 47.5735 105.405L47.1565 43.3868C47.1238 38.5125 52.2689 35.3378 56.6107 37.5533L116.594 68.1605C121.286 70.5547 121.331 77.2429 116.671 79.6999Z" fill="white" stroke="black" strokeWidth="7"/>
+            </Svg>
+        </Animated.View>
+    )
+}
+
+export default function PlayButton({ size=100, onClick=()=>{}, }) {
+    const [play, setPlay] = useState(true)
+    const style = {
+        size: {
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+        },
+    }
+    const onPress = () => {
+        setPlay(!play)
+        onClick()
+    }
+
+    return (
+        <TouchableOpacity {...{ onPress }}>
+            <View  style={[
+                styles.container,
+                style.size,
+            ]}>
+                <Icon />
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    }
+})
--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,4 @@
 
 export { default as TextButton } from './TextButton'
 export { default as RadioGroup } from './RadioGroup'
+export { default as PlayButton } from './PlayButton'
