323f590167b3e90db9a8b7956924d849d447d758
patrickfv <patrick.fv@outlook.com>
2021-02-08 01:02:37 -0300

    remove svg

--- app/components/PlayButton.jsx
+++ app/components/PlayButton.jsx
@@ -1,40 +1,52 @@
 import React, { useState, } from 'react'
-import { View, StyleSheet, TouchableOpacity, } from 'react-native'
-import Svg, { Path, Circle, } from 'react-native-svg'
+import { View, StyleSheet, TouchableOpacity, Dimensions } from 'react-native'
 import { useSharedValue, useAnimatedStyle, } from 'react-native-reanimated'
 import Animated from 'react-native-reanimated'
 
-function Icon() {
+export default function PlayButton({ size=100, onClick=()=>{}, }) {
+    const [play, setPlay] = useState(true)
     const offsetY = useSharedValue(0)
     const animatedStyle = useAnimatedStyle(() => {
         return {
-            transform: [{ rotateY: `${offsetY.value}deg` }]
+            transform: [
+                //{ rotateY: `${offsetY.value}deg` },
+                //{ perspective: 200 },
+                { translateX: 10 },
+                //{ rotateZ: '90deg' }
+            ]
         }
     })
     const onTouchStart = () => {
         offsetY.value = Math.random() * 255
     }
-
-    return (
-        <Animated.View style={animatedStyle} {...{ onTouchStart }} >
-            <Svg width="148" height="148" viewBox="0 0 148 148" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <Circle cx="74" cy="74" r="70.5" fill="white" stroke="black" strokeWidth="7"/>
-                <Path d="M116.594 68.1605C121.286 70.5547 121.331 77.2429 116.671 79.6999L57.1053 111.111C52.7936 113.385 47.6063 110.279 47.5735 105.405L47.1565 43.3868C47.1238 38.5125 52.2689 35.3378 56.6107 37.5533L116.594 68.1605Z" fill="white" stroke="black" strokeWidth="7"/>
-                <Path d="M116.671 79.6999L57.1053 111.111C52.7936 113.385 47.6063 110.279 47.5735 105.405L47.1565 43.3868C47.1238 38.5125 52.2689 35.3378 56.6107 37.5533L116.594 68.1605C121.286 70.5547 121.331 77.2429 116.671 79.6999Z" fill="white" stroke="black" strokeWidth="7"/>
-            </Svg>
-        </Animated.View>
-    )
-}
-
-export default function PlayButton({ size=100, onClick=()=>{}, }) {
-    const [play, setPlay] = useState(true)
-    const style = {
+    const style = StyleSheet.create({
         size: {
             width: size,
             height: size,
-            borderRadius: size / 2,
         },
-    }
+        triangle: {
+            width: 0,
+            height: 0,
+            backgroundColor: 'transparent',
+            borderStyle: 'solid',
+            
+            borderTopWidth: size / 3,
+            borderBottomWidth: size / 3,
+            borderLeftWidth: size - size / 2,
+            
+            borderTopColor: 'transparent',
+            borderBottomColor: 'transparent',
+            borderLeftColor: '#fff',
+        },
+        ring: {
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+            borderWidth: 5,
+            backgroundColor: 'transparent',
+            borderColor: '#fff',
+        },
+    })
     const onPress = () => {
         setPlay(!play)
         onClick()
@@ -44,8 +56,10 @@ export default function PlayButton({ size=100, onClick=()=>{}, }) {
         <TouchableOpacity {...{ onPress }}>
             <View  style={[
                 styles.container,
+                style.ring
             ]}>
-                <Icon />
+                <Animated.View style={[style.triangle, animatedStyle]} {...{ onTouchStart }} >
+                </Animated.View>
             </View>
         </TouchableOpacity>
     )
@@ -55,6 +69,5 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#fff',
-    }
+    },
 })
