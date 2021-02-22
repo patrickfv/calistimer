c831d37544789a75d8b14aba65dcbf56377e60fc
patrickfv <patrick.fv@outlook.com>
2021-02-21 23:31:57 -0300

    refactor styles

--- app/components/PlayButton.jsx
+++ app/components/PlayButton.jsx
@@ -1,29 +1,44 @@
 import React, { useState, useEffect, } from 'react'
-import { View, StyleSheet, TouchableWithoutFeedback, } from 'react-native'
+import { View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import FlipCard from './FlipCard'
 
 export default function PlayButton({ size=100, onClick=()=>{}, }) {
-    const [play, setPlay] = useState(false)
+    const [play, setPlay] = useState(0)
     
+    const styleCircle = {
+        justifyContent: 'center',
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+    }
     useEffect(() => {
-       
     }, [play])
 
     const onPress = () => {
-        setPlay(!play)
+        setPlay(play => (play === 0 ? 1 : 0))
         onClick()
+    }
+    const Side = ({ title, color='transparent', }) => {
+
+        return (
+            <View style={[
+                styleCircle,
+                { backgroundColor: color, width: '100%', height: '100%', },
+            ]}>
+                <Text style={styles.text}>{ title }</Text>
+            </View>
+        )
     }
 
     return (
         <TouchableWithoutFeedback {...{ onPress }}>
-            <View  style={[
-                styles.container,
-                { 
-                    width: size, 
-                    height: size, 
-                    borderRadius: (size / 2), 
-                },
-            ]}>
-
+            <View  style={styles.container}>
+                <FlipCard 
+                    side={play}
+                    style={styleCircle}
+                    front={<Side title="Play" color="blue" />}
+                    back={<Side title="Pause" color="blue" />}
+                />
             </View>
         </TouchableWithoutFeedback>
     )
@@ -32,9 +47,14 @@ export default function PlayButton({ size=100, onClick=()=>{}, }) {
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
-        justifyContent: 'space-around',
+        justifyContent: 'center',
         borderWidth: 5,
         backgroundColor: 'transparent',
-        borderColor: '#fff',
+        borderColor: 'transparent',
+    },
+    text: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 20,
     },
 })
