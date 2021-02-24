1ec34723a4c77869939ea90e0dcda97d93b43858
patrickfv <patrick.fv@outlook.com>
2021-02-23 23:03:57 -0300

    create ProgressBar component

---
+++ app/components/ProgressBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, StyleSheet, } from 'react-native'
+
+export default function ProgressBar() {
+    return (
+        <View style={styles.container} />
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
--- app/components/index.js
+++ app/components/index.js
@@ -2,3 +2,4 @@
 export { default as TextButton } from './TextButton'
 export { default as RadioGroup } from './RadioGroup'
 export { default as PlayButton } from './PlayButton'
+export { default as ProgressBar } from './ProgressBar'
