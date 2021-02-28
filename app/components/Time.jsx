0bc43df5f8fe8bf11046c0ef8a51ad73f23e0b2c
patrickfv <patrick.fv@outlook.com>
2021-02-28 20:20:23 -0300

    create Time component

---
+++ app/components/Time.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, StyleSheet, } from 'react-native'
+
+export default function Time() {
+    return (
+        <View style={styles.container} />
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
