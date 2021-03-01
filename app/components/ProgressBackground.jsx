8b58adedc7d213c70e18d20cfb821e322509d1c0
patrickfv <patrick.fv@outlook.com>
2021-02-28 22:14:15 -0300

    implement ProgressBackground

--- app/components/ProgressBackground.jsx
+++ app/components/ProgressBackground.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { View, StyleSheet, } from 'react-native'
 
-export default function ProgressBackground() {
+export default function ProgressBackground({ children, percentage, }) {
+    
     return (
-        <View style={styles.container} />
+        <View style={styles.container}>
+            <View style={{ flex: 1, }}>
+                <View style={{ flex: 1 - percentage / 100, backgroundColor: '#E40039', }} />
+                <View style={{ flex: percentage / 100, backgroundColor: '#2A0E12', }} />
+            </View>
+            <View style={styles.children}>
+                { children }
+            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    children: {
+        position: 'absolute',
+        bottom: -50,
+        right: 50,
+        height: 100,
+        width: 100,
     },
 })
