dc97e63f4d52d0a91a5366c39b28e48929ed7cce
patrickfv <patrick.fv@outlook.com>
2021-02-28 22:15:20 -0300

    implement Running screen

--- app/screens/EMONScreen/Running.jsx
+++ app/screens/EMONScreen/Running.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { View, StyleSheet, } from 'react-native'
 
+import { ProgressBackground, Time } from '../../components'
+
 export default function Running() {
     return (
-        <View style={styles.container} />
+        <View style={styles.container}>
+            <ProgressBackground percentage={60}>
+                <Time time={2400}/>
+            </ProgressBackground>
+        </View>
     )
 }
 
