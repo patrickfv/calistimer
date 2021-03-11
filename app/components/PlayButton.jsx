30b71475a55697c81f45f73f781b133480e0e571
patrickfv <patrick.fv@outlook.com>
2021-03-11 01:57:01 -0300

    add button with out animation

--- app/components/PlayButton.jsx
+++ app/components/PlayButton.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, } from 'react'
-import { View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native'
+import { View, StyleSheet, TouchableWithoutFeedback, Text, } from 'react-native'
 import AnimatedFlip from './AnimatedFlip'
 
 export default function PlayButton({ size=100, onClick=()=>{}, }) {
     const [play, setPlay] = useState(0)
-    
+
     const styleCircle = {
         justifyContent: 'center',
         width: size,
@@ -18,32 +18,37 @@ export default function PlayButton({ size=100, onClick=()=>{}, }) {
         setPlay(play => (play === 0 ? 1 : 0))
         onClick()
     }
-    const Side = ({ title, bgColor='transparent', color='white' }) => {
-
-        return (
-            <View style={[
-                styleCircle,
-                { backgroundColor: bgColor, width: '100%', height: '100%', },
+    const Side = ({ title, bgColor='transparent', color='white' }) => (
+        <View style={[
+            styleCircle,
+            { backgroundColor: bgColor, width: '100%', height: '100%', },
+        ]}>
+            <Text style={[
+                styles.text,
+                { color: color }
             ]}>
-                <Text style={[
-                    styles.text, 
-                    { color: color }
-                ]}>
-                    { title }
-                </Text>
-            </View>
-        )
-    }
+                { title }
+            </Text>
+        </View>
+    )
+
+    const ButtonWithoutAnimation = () => (
+        <View style={[styleCircle, { backgroundColor: 'white', alignItems: 'center', }]}>
+            <Text style={{ fontSize: 21, fontWeight: 'bold', }}>Play</Text>
+        </View>
+    )
 
     return (
         <TouchableWithoutFeedback {...{ onPress }}>
             <View  style={styles.container}>
-                <AnimatedFlip 
-                    side={play}
-                    style={styleCircle}
-                    front={<Side title="Play" color="black" bgColor="white" />}
-                    back={<Side title="Pause" color="black" bgColor="white" />}
-                />
+                {
+                //<AnimatedFlip
+                //  side={play}
+                //  style={styleCircle}
+                //  front={<Side title="Play" color="black" bgColor="white" />}
+                //  back={<Side title="Pause" color="black" bgColor="white" />} />
+                <ButtonWithoutAnimation />
+                }
             </View>
         </TouchableWithoutFeedback>
     )
