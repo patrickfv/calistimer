9c1e7d43b6d9f2aecc799b7eba880d702252764f
patrickfv <patrick.fv@outlook.com>
2021-03-14 20:47:42 -0300

    implement timeout sound

--- app/screens/EMONScreen/Running.jsx
+++ app/screens/EMONScreen/Running.jsx
@@ -1,47 +1,69 @@
 import React, { useState, useEffect, } from 'react'
 import { View, StyleSheet, Text, Dimensions, } from 'react-native'
+import { Audio, } from 'expo-av'
 
 import { ProgressBackground, Time, ProgressBar, } from '../../components'
 
 const { width, } = Dimensions.get('window')
 
-export default function Running({ countdown=true, time=60, }) {
+export default function Running({ countdown=false, time=60, alert=0, run=false, }) {
     const incrementOrDecrement = countdown ? 0 : time
+    const [sound, setSound] = useState()
     const [count, setCount] = useState(incrementOrDecrement)
-    const [running, setRunning] = useState(true)
+    const [running, setRunning] = useState(run)
+    // const [timeout, setTimeout] = useState(5)
 
+    const playSound = async () => {
+        const { sound, } = await Audio.Sound.createAsync(require('../../assets/alert.wav'))
+        await sound.playAsync()
+    }
+
+    // const clearSound = async () => await sound.unloadAsync()
+    
     const increment = () => {
         const interval = setInterval(() => {
-            if(count < time) {
-                setCount(parseInt(count + 1))
+            if(running) {
+                if(count < time) {
+                    setCount(parseInt(count + 1))
+                } else {
+                    clearInterval(interval)
+                }
             } else {
-                clearInterval(interval)
+                playSound()
             }
         }, 1000)
         return interval
     }
-
-    const decrement = () => {
+    
+    const decrement = () => {    
         const interval = setInterval(() => {
-            if(count > 0) {
-                setCount(parseInt(count - 1))
+            if(running) {
+                if(count > 0) {
+                    setCount(parseInt(count - 1))
+                } else {
+                    clearInterval(interval)
+                }
             } else {
-                clearInterval(interval)
+                playSound()
             }
-            console.log(parseInt(count / time * 100))
         }, 1000)
         return interval
     }
 
     useEffect(() => {
-        const interval = countdown ? increment() : decrement()
+        var interval = countdown ? increment() : decrement()
         return () => clearInterval(interval)
-    }, [count])
+    }, [running, count])
 
-    // useEffect(() => {
-    //
-    // }, [running])
-    // (count % 60 / 60 * 100)
+    useEffect(() => {
+        const timeout = setInterval(() => {
+            setRunning(true)
+            console.log(running)
+            return clearTimeout(timeout)
+        }, 5000)
+        return () => clearTimeout(timeout)
+    }, [])
+    
     return (
         <ProgressBackground percentage={parseInt(count / time * 100)}>
             <View style={styles.container}>
