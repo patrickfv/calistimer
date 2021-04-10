c23942235371f918e7b7a4c54e1ef6eb308ba19b
patrickfv <patrick.fv@outlook.com>
2021-04-10 00:09:17 -0300

    add animation ProgressBackground

--- app/components/ProgressBackground.jsx
+++ app/components/ProgressBackground.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { View, StyleSheet, } from 'react-native'
+import React, { useEffect, useState, } from 'react'
+import { View, StyleSheet, Animated, } from 'react-native'
 
 export default function ProgressBackground({ children, percentage, }) {
+    const [animatedHeight, _] = useState(new Animated.Value(0))
+    const height1 = animatedHeight.interpolate({
+        inputRange: [0, 100],
+        outputRange: ['0%', '100%'],
+        extrapolate: 'clamp',
+    })
+    const height2 = animatedHeight.interpolate({
+        inputRange: [0, 100],
+        outputRange: ['100%', '0%'],
+        extrapolate: 'clamp',
+    })
+
+    useEffect(() => {
+        Animated.timing(animatedHeight, {
+            toValue: percentage,
+            duration: 500,
+            useNativeDriver: false,
+        }).start()
+    }, [percentage])
 
     return (
         <View style={styles.container}>
             <View style={{ flex: 1, }}>
-                <View style={{ flex: 1 - percentage / 100, backgroundColor: '#E40039', }} />
-                <View style={{ flex: percentage / 100, backgroundColor: '#2A0E12', }} />
+                <Animated.View style={{ height: height2, backgroundColor: '#E40039', }} />
+                <Animated.View style={{ height: height1, backgroundColor: '#2A0E12', }} />
             </View>
             <View style={styles.children}>
                 { children }
