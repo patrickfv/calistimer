2d6d8c02a11730e34e26ee0b61700861cff4f131
patrickfv <patrick.fv@outlook.com>
2021-04-10 19:03:10 -0300

    remove PlayButton

--- app/components/index.js
+++ app/components/index.js
@@ -1,7 +1,7 @@
 
 export { default as TextButton } from './TextButton'
 export { default as RadioGroup } from './RadioGroup'
-export { default as PlayButton } from './PlayButton'
 export { default as ProgressBar } from './ProgressBar'
 export { default as ProgressBackground } from './ProgressBackground'
 export { default as Time } from './Time'
+export { default as StaticButton } from './StaticButton'
