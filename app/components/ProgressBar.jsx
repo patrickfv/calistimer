f5da5c491f41405642f42c8e844f26f34535969c
patrickfv <patrick.fv@outlook.com>
2021-04-10 00:09:27 -0300

    add animation ProgressBar

--- app/components/ProgressBar.jsx
+++ app/components/ProgressBar.jsx
@@ -1,11 +1,25 @@
-import React, { useEffect, useRef, } from 'react'
-import { Dimensions, View, } from 'react-native'
+import React, { useEffect, useState, } from 'react'
+import { Animated, Dimensions, } from 'react-native'
 
 export default function ProgressBar({ percentage, height=3, color='white', style, }) {
-    const width = Dimensions.get('window').width / 100 * percentage
+    // const width = Dimensions.get('window').width / 100 * percentage
+    const [animatedWidth, _] = useState(new Animated.Value(0))
+    const width = animatedWidth.interpolate({
+        inputRange: [0, 100],
+        outputRange: ['0%', '100%'],
+        extrapolate: 'clamp',
+    })
+
+    useEffect(() => {
+        Animated.timing(animatedWidth, {
+            toValue: percentage,
+            duration: 500,
+            useNativeDriver: false,
+        }).start()
+    }, [percentage])
     
     return (
-        <View style={[
+        <Animated.View style={[
             style,
             { height, width, backgroundColor: color, }
         ]}/>
