2a28bc339812562e2644adb917d5eefcb0e82fbd
patrickfv <patrick.fv@outlook.com>
2021-04-24 19:24:13 -0300

    create amrap

---
+++ app/screens/AMRAPScreen/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, } from 'react-native'
+
+export const AMRAP_SCREEN_NAME = 'AMRAP_SCREEN'
+
+export default function AMRAPScreen() {
+    return (
+        <View>
+        </View>
+    )
+}
